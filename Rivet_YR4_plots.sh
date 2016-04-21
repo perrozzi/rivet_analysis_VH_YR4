e8a1511b5a00dd55ff452f354c432c3b832e1cab
perrozzi <perrozzi@cern.ch>
2016-04-21 15:12:21 +0200

    mega update + text

--- Rivet_YR4_plots.sh
+++ Rivet_YR4_plots.sh
@@ -14,54 +14,79 @@ cd -
 
 OUTDIR=Plots/
 RIVETDIR=./
+export RIVET_ANALYSIS_PATH=$RIVETDIR
+
 
 # HZ(->LL)
 # STABLE
-HIST_ZLL_HSTABLE_POWHEG_PY6=powheg/HZLL_stableHiggs.yoda
-HIST_ZLL_HSTABLE_MG5AMCNLO_PY8=mg5amcnlo/ZH_PY8_5M.yoda
-HIST_ZLL_HSTABLE_MG5AMCNLO_HW7=mg5amcnlo/ZHjets_HW7-FxFx.yoda
-HIST_ggHZLL_HSTABLE_MG5AMCNLO_PY8=mg5amcnlo/ggZH1M_merged_hadronize_Hnotdecayed.yoda
-# UNSTABLE
-HIST_ZLL_HDECAYED_POWHEG_PY6=powheg/HZLL_decayedHiggs.yoda
-
-# Make plots
-export RIVET_ANALYSIS_PATH=$RIVETDIR
-
-# Z->LL
-
-# rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL \
-# $HIST_ZLL_POWHEG:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=red:Title='$HZ(\to ll)j$  POWHEG-BOX'
-
-rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL_HSTABLE_POW_vs_aMCNLO_nonorm \
-$HIST_ZLL_HSTABLE_POWHEG_PY6:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=red:Title='ZllH POWHEG-BOX+PY6' \
-$HIST_ZLL_HSTABLE_MG5AMCNLO_PY8:ErrorBands=1:ErrorBandColor=green:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=black:Title='ZllH MG5AMCNLO+PY8' \
-$HIST_ggHZLL_HSTABLE_MG5AMCNLO_PY8:ErrorBands=1:ErrorBandColor=cyan:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=blue:Title='ggZllH MG5AMCNLO+PY8'
-
-rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL_HSTABLE_POW_vs_aMCNLO_norm \
-$HIST_ZLL_HSTABLE_POWHEG_PY6:NormalizeToIntegral=1:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=red:Title='ZllH POWHEG-BOX+PY6' \
-$HIST_ZLL_HSTABLE_MG5AMCNLO_PY8:NormalizeToIntegral=1:ErrorBands=1:ErrorBandColor=green:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=black:Title='ZllH MG5AMCNLO+PY8' \
-$HIST_ggHZLL_HSTABLE_MG5AMCNLO_PY8:NormalizeToIntegral=1:ErrorBands=1:ErrorBandColor=cyan:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=blue:Title='ggZllH MG5AMCNLO+PY8'
-
-# rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL_HSTABLE_aMCNLO_PY_vs_HW7 \
-# $HIST_ZLL_HSTABLE_MG5AMCNLO_PY8:NormalizeToIntegral=1:ErrorBands=1:ErrorBandColor=green:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=black:Title='MG5AMCNLO+PY8' \
-# $HIST_ZLL_HSTABLE_MG5AMCNLO_HW7:NormalizeToIntegral=1:ErrorBands=1:ErrorBandColor=blue:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=blue:LineColor=cyan:Title='MG5AMCNLO+HW7'
-
-# rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL_HUNDECAYED \
-# $HIST_ZLL_POWHEG:ErrorBands=1:ErrorBandColor=blue:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=blue:LineColor=blue:Title='$HZ(\to ll)j$  POWHEG-BOX' \
-# $HIST_ZLL_HAD_POWHEG:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=red:Title='$H(\to b\bar{b})Z(\to ll)j$  POWHEG-BOX'
-
+HIST_ZLL_HSTABLE_POWHEG_PY6=powheg/HZLL_stableHiggs_new.yoda
+HIST_ZLL_HSTABLE_MG5AMCNLO_PY8=mg5amcnlo/ZHjets_PY8-FxFx_Hnotdecayed_new.yoda
+HIST_ZLL_HSTABLE_MG5AMCNLO_HW7=mg5amcnlo/ZHjets_HW7-FxFx_Hnotdecayed_new.yoda
+HIST_ggHZLL_HSTABLE_MG5AMCNLO_PY8_UNMERGED=mg5amcnlo/gghz_MC_ZllHbb_undecayed_new.yoda
+# HIST_ggHZLL_HSTABLE_MG5AMCNLO_PY8=mg5amcnlo/ggZH1M_merged_hadronize_Hnotdecayed.yoda
+# DECAYED
+HIST_ZLL_HDECAYED_POWHEG_PY6=powheg/HZLL-decayedHiggs_new.yoda
+HIST_ZLL_HDECAYED_MG5AMCNLO_HW7=mg5amcnlo/ZHjets_HW7-FxFx_Hdecayed_new.yoda
+HIST_ZLL_HDECAYED_MG5AMCNLO_PY8=mg5amcnlo/ZllHbb_PY8-FxFx_Hdecayed_new.yoda
 
 # HZ(->NN)
-# HIST_ZNN_POWHEG=$RUNDIR/Run-yr4-nn-scalevar-rivet/HZNN.yoda
-# HIST_ZNN_HAD_POWHEG=$RUNDIR/Run-yr4-nn-scalevar-rivet/HZNN-HAD.yoda
+# STABLE
+HIST_ZNN_HSTABLE_POWHEG_PY6=powheg/HZNN_stableHiggs_new.yoda
+# HIST_ZNN_HSTABLE_MG5AMCNLO_PY8=mg5amcnlo/ZHjets_PY8-FxFx_Hnotdecayed_new.yoda
+HIST_ZNN_HSTABLE_MG5AMCNLO_HW7=mg5amcnlo/ZnunuHjets_HW7-FxFx_Hnotdecayed_new.yoda
+# HIST_ggHZNN_HSTABLE_MG5AMCNLO_PY8=mg5amcnlo/ggZH1M_merged_hadronize_Hnotdecayed.yoda
+# HIST_ggHZNN_HSTABLE_MG5AMCNLO_PY8_UNMERGED=mg5amcnlo/gghz_MC_ZNNHbb_undecayed_new.yoda
+# DECAYED
+HIST_ZNN_HDECAYED_POWHEG_PY6=powheg/HZNN-decayedHiggs_new.yoda
+HIST_ZNN_HDECAYED_MG5AMCNLO_HW7=mg5amcnlo/ZnunuHjets_HW7-FxFx_Hdecayed_new.yoda
+HIST_ZNN_HDECAYED_MG5AMCNLO_PY8=mg5amcnlo/ZnnH_PY8-FxFx_Hnotdecayed_new.yoda
 
-# rivet-mkhtml --cm -o $OUTDIR/YR4_HZNN \
-# $HIST_ZNN_POWHEG:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=red:Title='$HZ(\to \nu\nu)j$  POWHEG-BOX'
+# HWP-decayedHiggs_new
+# HWM-decayedHiggs_new
+# HWP_stableHiggs_new
+# HWM_stableHiggs_new
+# WlnuH_PY8-FxFx_Hnotdecayed_new
+
+# Make plots
+
+# YR4_HZLL_HDECAYED_POW_vs_aMCNLO
+rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL_HDECAYED_POW_vs_aMCNLO \
+$HIST_ZLL_HDECAYED_POWHEG_PY6:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=orange:Title='ZllH POWHEG+PY6' \
+$HIST_ZLL_HDECAYED_MG5AMCNLO_PY8:ErrorBands=1:ErrorBandColor=green:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=green:Title='ZllH MG5_aMC+PY8' \
+$HIST_ZLL_HDECAYED_MG5AMCNLO_HW7:ErrorBands=1:ErrorBandColor=cyan:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=blue:LineColor=blue:Title='ZllH MG5_aMC+HW7' \
+
+# YR4_HZLL_HSTABLE_POW_vs_aMCNLO
+rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL_HSTABLE_POW_vs_aMCNLO \
+$HIST_ZLL_HSTABLE_POWHEG_PY6:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=orange:Title='ZllH POWHEG+PY6' \
+$HIST_ZLL_HSTABLE_MG5AMCNLO_PY8:ErrorBands=1:ErrorBandColor=green:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=green:Title='ZllH MG5_aMC+PY8' \
+$HIST_ZLL_HSTABLE_MG5AMCNLO_HW7:ErrorBands=1:ErrorBandColor=cyan:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=blue:LineColor=blue:Title='ZllH MG5_aMC+HW7' \
+
+# YR4_HZLL_vs_ggHZLL_HSTABLE_aMCNLO
+rivet-mkhtml --cm -o $OUTDIR/YR4_HZLL_vs_ggHZLL_HSTABLE_aMCNLO \
+$HIST_ZLL_HSTABLE_MG5AMCNLO_PY8:ErrorBands=1:ErrorBandColor=green:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=green:Title='ZllH MG5_aMC+PY8' \
+$HIST_ggHZLL_HSTABLE_MG5AMCNLO_PY8_UNMERGED:ErrorBands=1:ErrorBandColor=gray:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=gray:LineColor=gray:Title='ggZllH MG5_aMC+PY8'
+
+# YR4_HZNN_HSTABLE_POW_vs_aMCNLO
+rivet-mkhtml --cm -o $OUTDIR/YR4_HZNN_HSTABLE_POW_vs_aMCNLO \
+$HIST_ZNN_HSTABLE_POWHEG_PY6:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=orange:Title='ZNNH POWHEG+PY6' \
+$HIST_ZNN_HSTABLE_MG5AMCNLO_HW7:ErrorBands=1:ErrorBandColor=cyan:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=blue:LineColor=blue:Title='ZNNH MG5_aMC+HW7' \
+
+# YR4_HZNN_HDECAYED_POW_vs_aMCNLO
+rivet-mkhtml --cm -o $OUTDIR/YR4_HZNN_HDECAYED_POW_vs_aMCNLO \
+$HIST_ZNN_HDECAYED_POWHEG_PY6:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=orange:Title='ZNNH POWHEG+PY6' \
+$HIST_ZNN_HDECAYED_MG5AMCNLO_PY8:ErrorBands=1:ErrorBandColor=green:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=green:LineColor=green:Title='ZNNH MG5_aMC+PY8' \
+$HIST_ZNN_HDECAYED_MG5AMCNLO_HW7:ErrorBands=1:ErrorBandColor=cyan:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=blue:LineColor=blue:Title='ZNNH MG5_aMC+HW7' \
 
 
-# rivet-mkhtml --cm -o $OUTDIR/YR4_HZNN_HAD \
-# $HIST_ZNN_HAD_POWHEG:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=red:Title='$H(\to b\bar{b})Z(\to \nu\nu)j$  POWHEG-BOX'
+cp -r Plots/YR4_HZLL_HDECAYED_POW_vs_aMCNLO ~/www/rivet_analysis_VH_YR4/
+cp -r Plots/YR4_HZLL_HSTABLE_POW_vs_aMCNLO ~/www/rivet_analysis_VH_YR4/
+cp -r Plots/YR4_HZLL_vs_ggHZLL_HSTABLE_aMCNLO ~/www/rivet_analysis_VH_YR4/
+cp -r Plots/YR4_HZNN_HSTABLE_POW_vs_aMCNLO ~/www/rivet_analysis_VH_YR4/
+cp -r Plots/YR4_HZNN_HDECAYED_POW_vs_aMCNLO ~/www/rivet_analysis_VH_YR4/
 
-# rivet-mkhtml --cm -o $OUTDIR/YR4_HZNN_COMP \
-# $HIST_ZNN_POWHEG:ErrorBands=1:ErrorBandColor=blue:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=blue:LineColor=blue:Title='$HZ(\to \nu\nu)j$  POWHEG-BOX' \
-# $HIST_ZNN_HAD_POWHEG:ErrorBands=1:ErrorBandColor=orange:ErrorBandOpacity=0.5:RatioPlotErrorBandColor=orange:LineColor=red:Title='$H(\to b\bar{b})Z(\to \nu\nu)j$  POWHEG-BOX'
+cp -r /afs/cern.ch/user/p/perrozzi/www/dyres/index.php ~/www/rivet_analysis_VH_YR4/YR4_HZLL_HDECAYED_POW_vs_aMCNLO/MC_ZllHbb_decayed
+cp -r /afs/cern.ch/user/p/perrozzi/www/dyres/index.php ~/www/rivet_analysis_VH_YR4/YR4_HZLL_HSTABLE_POW_vs_aMCNLO/MC_ZllHbb_undecayed
+cp -r /afs/cern.ch/user/p/perrozzi/www/dyres/index.php ~/www/rivet_analysis_VH_YR4/YR4_HZLL_vs_ggHZLL_HSTABLE_aMCNLO/MC_ZllHbb_undecayed
+cp -r /afs/cern.ch/user/p/perrozzi/www/dyres/index.php ~/www/rivet_analysis_VH_YR4/YR4_HZNN_HSTABLE_POW_vs_aMCNLO/MC_ZnunuHbb_undecayed
+cp -r /afs/cern.ch/user/p/perrozzi/www/dyres/index.php ~/www/rivet_analysis_VH_YR4/YR4_HZNN_HDECAYED_POW_vs_aMCNLO/MC_ZnunuHbb_decayed
+
